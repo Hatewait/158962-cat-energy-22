3ca8b5d186598aa52d35332648b527932f7c8149
Anastasia Migaeva <pandora1983@yandex.ru>
2021-05-06 15:31:05 +0300

    pixel, gulp

--- source/js/main.js
+++ source/js/main.js
@@ -1,13 +1,11 @@
 'use strict'
 
 const navMain = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.nav-toggle');
+const navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', (evt) => {
   evt.preventDefault();
   navMain.classList.toggle('main-nav--open');
-  // navToggle.classList.toggle('nav-toggle--close');
-  // navToggle.classList.toggle('nav-toggle--open');
 })
